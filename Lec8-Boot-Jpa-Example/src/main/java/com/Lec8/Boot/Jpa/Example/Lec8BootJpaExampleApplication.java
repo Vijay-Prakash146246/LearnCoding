eaced90156df07f520a5d78d6fb34eaeed241e6d
Vijay-Prakash146246 <vp146246@gmail.com>
2023-08-29 12:24:20 +0530

    Custom created Finder methods get added in repository interface

--- Lec8-Boot-Jpa-Example/src/main/java/com/Lec8/Boot/Jpa/Example/Lec8BootJpaExampleApplication.java
+++ Lec8-Boot-Jpa-Example/src/main/java/com/Lec8/Boot/Jpa/Example/Lec8BootJpaExampleApplication.java
@@ -16,47 +16,59 @@ public class Lec8BootJpaExampleApplication
 	{
 		ApplicationContext context= SpringApplication.run(Lec8BootJpaExampleApplication.class, args);
 		UserRepository repository1 = context.getBean(UserRepository.class);
-		User user=new User();
-		user.setName("Vijay Prakash Singh");
-		user.setCity("Nawada , Bihar");
-		user.setStatus("I am java programmer");
+//		User user=new User();
+//		user.setName("Vijay Prakash Singh");
+//		user.setCity("Nawada , Bihar");
+//		user.setStatus("I am java programmer");
+//
+//		User user1=new User();
+//		user1.setName("Vijay Prakash");
+//		user1.setCity("Nawada");
+//		user1.setStatus("I am a programmer");
+//		//saving single user
+//		repository1.save(user1);
+//		System.out.println("Saved User : "+user1);
+//		//saving single user
+//		repository1.save(user);
+//		System.out.println("Saved User : "+user);
+//
+//
+//
+//		//L13 CRUD Operation using JPA and Spring Boot
+//		//save multiple object
+//		List<User>users=List.of(user1,user);
+//		Iterable<User>result = repository1.saveAll(users);
+//		result.forEach(
+//				user2 -> {
+//					System.out.println(user2);
+//				}
+//		);
+//
+//
+//		//update the user of id 2
+//		Optional<User> user2 = repository1.findById(2);
+//		user2.get().setName("Amit Kumar Singh");
+//		repository1.save(user2.get());
+//		System.out.println(user2.get());
+//
+//		//get the data
+//		List<User> users1= repository1.findAll();
+//		users1.forEach(System.out::println);
+//
+//		//delete data
+//		repository1.deleteById(1);
 
-		User user1=new User();
-		user1.setName("Vijay Prakash");
-		user1.setCity("Nawada");
-		user1.setStatus("I am a programmer");
-		//saving single user
-		repository1.save(user1);
-		System.out.println("Saved User : "+user1);
-		//saving single user
-		repository1.save(user);
-		System.out.println("Saved User : "+user);
-
-
-
-		//L13 CRUD Operation using JPA and Spring Boot
-		//save multiple object
-		List<User>users=List.of(user1,user);
-		Iterable<User>result = repository1.saveAll(users);
-		result.forEach(
-				user2 -> {
-					System.out.println(user2);
-				}
-		);
-
-
-		//update the user of id 2
-		Optional<User> user2 = repository1.findById(2);
-		user2.get().setName("Amit Kumar Singh");
-		repository1.save(user2.get());
-		System.out.println(user2.get());
-
-		//get the data
-		List<User> users1= repository1.findAll();
+		//Lecture14 Custom Finder methods
+		List<User> users=repository1.findByName("Amit Kumar Singh");
+		users.forEach(System.out::println);
+		List<User> users1=repository1.findByNameAndCity("Amit Kumar Singh","Nawada , Bihar");
 		users1.forEach(System.out::println);
-
-		//delete data
-		repository1.deleteById(1);
+		List<User> users2=repository1.findByNameStartingWith("Amit");
+		users2.forEach(System.out::println);
+		List<User> users3=repository1.findByNameEndingWith("Singh");
+		users3.forEach(System.out::println);
+		List<User> users4=repository1.findByNameContaining("Kumar");
+		users4.forEach(System.out::println);
 	}
 
 }
--- Lec8-Boot-Jpa-Example/src/main/java/com/Lec8/Boot/Jpa/Example/repository/UserRepository.java
+++ Lec8-Boot-Jpa-Example/src/main/java/com/Lec8/Boot/Jpa/Example/repository/UserRepository.java
@@ -2,9 +2,25 @@ package com.Lec8.Boot.Jpa.Example.repository;
 
 import com.Lec8.Boot.Jpa.Example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer>
 {
+    //custom  finder method OR derived query methods
+    public List<User> findByName(String name);
+    public List<User> findByNameAndCity(String name, String city);
+    public List<User> findByNameStartingWith(String name);
+   public List<User> findByNameEndingWith(String name);
+    public List<User> findByNameContaining(String name);
+//    public List<User> findByNameLike(String name);
+//    public List<User> findByAgeLessThan(int age);
+//    public List<User> findByAgeGreaterThanEqual(int age);
+//    public List<User> findByAgeIn(Collection<Integer> ages);
+//    public List<User> findByNameOrderByAgeAsc(String name);
 }
