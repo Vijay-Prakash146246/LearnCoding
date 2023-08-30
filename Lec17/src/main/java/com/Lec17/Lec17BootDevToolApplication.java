1b639cbafa760c48257b8000556c912bf8dbba11
Vijay-Prakash146246 <vp146246@gmail.com>
2023-08-30 12:40:53 +0530

    L17 Magic of Spring Boot DevTools get added

---
+++ Lec17/src/main/java/com/Lec17/Lec17BootDevToolApplication.java
@@ -0,0 +1,13 @@
+package com.Lec17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec17BootDevToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec17BootDevToolApplication.class, args);
+	}
+
+}
---
+++ Lec17/src/test/java/com/Lec17/Lec17BootDevToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lec17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec17BootDevToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
